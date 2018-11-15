a57572469ca4da809ca8314c0b91f91c5e98d5af
Shijia Hu <shijiahu2016a@gmail.com>
2018-11-15 00:12:59 -0800

    add mock of fetch function

--- script2.test.js
+++ script2.test.js
@@ -30,3 +30,28 @@ it('calls swapi to get people with a promise', ()=>{
 		expect(data.results.length).toBeGreaterThan(5)
 	})
 })
+
+
+it('getpeople return counts and results', ()=>{
+	// mock the function of fetch to make it faster
+	// jest.fn()
+	const mockFetch = jest.fn()
+		.mockReturnValue(Promise.resolve({
+			json:() => Promise.resolve({
+				count: 87,
+				results: [0,1,2,3,4,5]
+			})
+	}))
+	expect.assertions(4)
+	return swapi.getPeoplePromise(mockFetch).then(data=>{
+		expect(mockFetch.mock.calls.length).toBe(1)
+		expect(mockFetch).toBeCalledWith('https://swapi.co/api/people')
+		expect(data.count).toEqual(87)
+		expect(data.results.length).toBeGreaterThan(5)
+	})
+})
+
+
+
+
+
